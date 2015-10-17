3ed67fdc1b7a8d4a3917ee1d9da0872f7a3674c7
Andreandc <A0344162@itesm.mx>
2015-10-17 13:08:23 -0500

    Create q1fibonacci.cpp

---
+++ q1fibonacci.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+
+long n;
+long fibonacci (long n){
+  long first=0, second=1, third=0;
+
+for (int i=0; i<n-1; i++)
+{
+  third=first+second;
+  first=second;
+  second=third;
+}
+  return third;
+}
+
+
+
+int main(){
+  cout<<"Da un número positivo para la serie Fibonacci: ";
+  cin>>n;
+    if (n>=0)
+    {
+      cout<<"Resultado: "<<fibonacci(n);
+    }
+    else
+    {
+      cout<<"Invalid";
+    }
+  return 0;
+}
